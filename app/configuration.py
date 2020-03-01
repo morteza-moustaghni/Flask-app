1d532437554812103adc82e0fbf63e9fb1a3ab07
Morteza <morteza.moustaghni@gmail.com>
2020-03-02 00:22:47 +0100

    Update configuration.py

--- app/configuration.py
+++ app/configuration.py
@@ -22,7 +22,7 @@ class Config(object):
 	#RECAPTCHA_PRIVATE_KEY = "6LffFNwSAAAAAO7UURCGI7qQ811SOSZlgU69rvv7"
 
 class ProductionConfig(Config):
-	import instance.config.py
+	from instance.config.py import *
 
 class DevelopmentConfig(Config):
 	DEBUG = True
