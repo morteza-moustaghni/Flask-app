138a5509524c51e742e3a87888068a47cafee710
Morteza <morteza.moustaghni@gmail.com>
2020-03-02 00:35:12 +0100

    Update configuration.py

--- app/configuration.py
+++ app/configuration.py
@@ -18,14 +18,7 @@ class Config(object):
 	#Get your reCaptche key on: https://www.google.com/recaptcha/admin/create
 	#RECAPTCHA_PUBLIC_KEY = "6LffFNwSAAAAAFcWVy__EnOCsNZcG2fVHFjTBvRP"
 	#RECAPTCHA_PRIVATE_KEY = "6LffFNwSAAAAAO7UURCGI7qQ811SOSZlgU69rvv7"
-
-class ProductionConfig(Config):
-	print("ARE YOU SURE???")
-
 class DevelopmentConfig(Config):
 	DEBUG = True
 	DATABASE_URI = 'sqlite:///application.db'
 	print("dev")
-
-class TestingConfig(Config):
-	TESTING = True
