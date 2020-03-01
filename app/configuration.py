b7ecc58c8fbf2408d730920b2e920502a72b9d5d
Morteza <morteza.moustaghni@gmail.com>
2020-03-02 00:31:36 +0100

    Update configuration.py

--- app/configuration.py
+++ app/configuration.py
@@ -3,8 +3,6 @@
 Python Aplication Template
 Licence: GPLv3
 """
-import sys
-
 
 class Config(object):
 	"""
@@ -22,7 +20,7 @@ class Config(object):
 	#RECAPTCHA_PRIVATE_KEY = "6LffFNwSAAAAAO7UURCGI7qQ811SOSZlgU69rvv7"
 
 class ProductionConfig(Config):
-	from instance.config.py import *
+	print("ARE YOU SURE???")
 
 class DevelopmentConfig(Config):
 	DEBUG = True
