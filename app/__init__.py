3d48426cfb7e87c7c738905dcb071b59305566bf
Morteza Moustaghni <morteza.moustaghni@gmail.com>
2020-03-22 18:13:55 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -38,6 +38,6 @@ def init_db():
     db.session.add(new_user)
     db.session.commit()
     print("first user created")
-    print(TestModel.query.all().name)
+    print(TestModel.query.all().first().name)
 init_db()
 
