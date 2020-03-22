bc79bcaffb973586eae8b55b8d75f1f6c3f97e2a
Morteza <morteza.moustaghni@gmail.com>
2020-03-22 20:19:50 +0100

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -2,14 +2,14 @@ from flask.ext.wtf import Form, TextField, TextAreaField, DateTimeField, Passwor
 from wtforms.validators import InputRequired, Email
 
 class ExampleForm(Form):
-	title = TextField(u'Title', validators = [Required()])
+	title = TextField(u'Title', validators = [InputRequired()])
 	content = TextAreaField(u'content')
 	date = DateTimeField(u'Date', format='%d/%m/%Y %H:%M')
 	#recaptcha = RecaptchaField(u'Recaptcha')
 
 class LoginForm(Form):
-	user = TextField(u'Username', validators = [Required()])
-	password = PasswordField(u'Password', validators = [Required()])
+	user = TextField(u'Username', validators = [InputRequired()])
+	password = PasswordField(u'Password', validators = [InputRequired()])
 
 class NewAccountForm(Form):
 	email = EmailField(u'Email', validators = [InputRequired(), Email()])
